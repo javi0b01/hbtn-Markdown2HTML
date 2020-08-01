0afaeb92ce16edf2ea0140c870e7b08a78a8c2c5
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-08-01 15:29:13 -0500

    Task 0

--- markdown2html.py
+++ markdown2html.py
@@ -4,16 +4,16 @@ Markdown is awesome! All your README.md are made in Markdown,
 but do you know how Github are rendering them?
 It’s time to code a Markdown to HTML!
 """
-import sys
-import os.path
+from sys import argv, stderr, exit
+from os import path
 if __name__ == "__main__":
-    size = len(sys.argv)
-    if size < 3:
-        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
-        sys.exit(1)
-    check = os.path.isfile('./README.md')
+    size = len(argv)
+    if size < 2:
+        print("Usage: ./markdown2html.py README.md README.html", file=stderr)
+        exit(1)
+    check = path.exists(argv[1])
     if (check is False):
-        sys.stderr.write("Missing {}\n".format(sys.argv[1]))
-        sys.exit(1)
-    sys.exit(0)
+        print("Missing {}".format(argv[1]), file=stderr)
+        exit(1)
+    exit(0)
 
