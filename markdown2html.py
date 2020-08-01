114572daa1f20af161ad704d8151ec53aecb9b51
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-08-01 16:41:00 -0500

    Task 1

--- markdown2html.py
+++ markdown2html.py
@@ -17,18 +17,17 @@ if __name__ == "__main__":
         exit(1)
     origin = argv[1]
     destination = argv[2]
-    with open(origin, "r") as o:
+    with open(origin, 'r') as o:
         lines = o.readlines()
         html_lines = []
         for line in lines:
-            amount = line.count("#")
-            tag_open = ("<h{}>".format(amount))
-            tag_close = ("</h{}>".format(amount))
-            sp = line.find(" ")
-            tag_content = line[sp+1: -1]
-            html_line = ("{}{}{}\n".format(tag_open, tag_content, tag_close))
-            html_lines.append(html_line)
-    with open(destination, "w") as d:
+            _list = line.split()
+            _fchar = _list[0]
+            _ochars = ' '.join(_list[1:])
+            if _fchar[0] == '#':
+                _hlevel = len(_fchar)
+                html_lines.append('<h{0}>{1}</h{0}>\n'.format(_hlevel, _ochars))
+    with open(destination, 'w') as d:
         d.writelines(html_lines)
     exit(0)
 
