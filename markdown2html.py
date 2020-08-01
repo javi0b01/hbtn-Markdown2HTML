3270edd86b22bde3a1ca2efc6b2453bbd174939c
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-08-01 15:34:58 -0500

    Task 0

--- markdown2html.py
+++ markdown2html.py
@@ -6,10 +6,9 @@ It’s time to code a Markdown to HTML!
 """
 from sys import argv, stderr, exit
 from os import path
-
 if __name__ == "__main__":
     size = len(argv)
-    if size < 2:
+    if size < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=stderr)
         exit(1)
     check = path.exists(argv[1])
