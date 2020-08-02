dd435219c0a05515758d1e1eb77f835cd90cb24e
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-08-01 22:35:40 -0500

    Task 2

--- markdown2html.py
+++ markdown2html.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
                 if ul == 1:
                     _lines.append('<ul>\n')
                 _lines.append('<li>{}</li>\n'.format(_ochars))
-            if ul > 1 and _fchar[0] != '-':
+            if (ul > 1 and _fchar[0] != '-') or (ul > 1 and line == mdlines[-1]):
                 _lines.append('</ul>\n')
     with open(argv[2], 'w') as html:
         html.writelines(_lines)
