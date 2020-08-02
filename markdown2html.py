e32027b80c7393106d4a99be7fe6ff7bd3eab796
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-08-01 20:14:27 -0500

    Task 1

--- markdown2html.py
+++ markdown2html.py
@@ -15,17 +15,17 @@ if __name__ == "__main__":
     if (check is False):
         print("Missing {}".format(argv[1]), file=stderr)
         exit(1)
-    html_lines = []
-    with open(argv[1], 'r') as o:
-        lines = o.readlines()
-        for line in lines:
+    with open(argv[1], 'r') as md:
+        mdlines = md.readlines()
+        _lines = []
+        for line in mdlines:
             _list = line.split()
             _fchar = _list[0]
             _ochars = ' '.join(_list[1:])
             if _fchar[0] == '#':
                 _hlevel = len(_fchar)
-                html_lines.append('<h{0}>{1}</h{0}>\n'.format(_hlevel, _ochars))
-    with open(argv[2], 'w') as d:
-        d.writelines(html_lines)
+                _lines.append('<h{0}>{1}</h{0}>\n'.format(_hlevel, _ochars))
+    with open(argv[2], 'w') as html:
+        html.writelines(_lines)
     exit(0)
 
